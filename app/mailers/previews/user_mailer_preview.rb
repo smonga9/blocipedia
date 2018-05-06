37ffccb4edb7d225f4adce0a8efd8c5b4466b122
Stutay Monga <Stutay.Monga9@gmail.com>
2018-05-06 18:19:08 -0400

    Updated Checkpoint 2-Part 2

--- app/mailers/previews/user_mailer_preview.rb
+++
@@ -1,2 +0,0 @@
-class UserMailerPreview < ActionMailer::Preview
-end
--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: "Stutay.Monga9@gmail.com"
-
-  def new_user(user)
-    @user = user
-    mail(to: user.email, subject: "Welcome to Blocipedia")
-  end
-end
