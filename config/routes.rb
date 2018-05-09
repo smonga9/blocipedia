41dd442e423a9ca6e868f68794e54df04f42ef0e
Stutay Monga <Stutay.Monga9@gmail.com>
2018-05-08 22:12:07 -0400

    Checkpoint 4 Completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  resources :wikis
+
   devise_for :users, controllers: { confirmations: 'confirmations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
