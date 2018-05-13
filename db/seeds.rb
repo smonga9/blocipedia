90d249b55c83d770445c073e2eec2239a9e0d395
Stutay Monga <Stutay.Monga9@gmail.com>
2018-05-13 12:24:00 -0500

    Checkpoint 6 - Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+# Create Users
+5.times do
+  User.create!(
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+
+# Create admin user
+1.times do
+  User.create!(
+  email:     'realtimschmidt@gmail.com',
+  password:  'helloworld',
+  role:       2
+  )
+end
+
+# Create standard user
+1.times do
+  User.create!(
+  email:    'schmidt.tim.j@gmail.com',
+  password: 'helloworld'
+  )
+end
+users = User.all
+
+ # Create Wikis
+ 50.times do
+   Wiki.create!(
+     user:   users.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ wikis = Wikis.all
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Wiki.count} wikis created"
--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,38 +1,36 @@
 module RandomData
-
-    def self.random_name
-        first_name = random_word.capitalize
-        last_name = random_word.capitalize
-        "#{first_name} #{last_name}"
-    end
-
-    def self.random_email
-        "#{random_word}@#{random_word}.#{random_word}"
-    end
-
     def self.random_paragraph
-        sentences = []
-        rand(4..6).times do
-            sentences << random_sentence
-        end
+      sentences = []
+      rand(4..6).times do
+        sentences << random_sentence
+      end
 
-        sentences.join(" ")
+      sentences.join(" ")
     end
-    
-    def self.random_sentence
-        strings = []
-        rand(3..8).times do
-            strings << random_word
-        end
 
-        sentence = strings.join(" ")
-        sentence.capitalize << "."
+    def self.random_sentence
+      strings = []
+      rand(3..8).times do
+        strings << random_word
+      end
+
+      sentence = strings.join(" ")
+      sentence.capitalize << "."
     end
 
     def self.random_word
-        letters = ('a'..'z').to_a
-        letters.shuffle!
-        letters[0,rand(3..8)].join
+      letters = ('a'..'z').to_a
+      letters.shuffle!
+      letters[0,rand(3..8)].join
     end
 
-end
+    def self.random_name
+         first_name = random_word.capitalize
+         last_name = random_word.capitalize
+         "#{first_name} #{last_name}"
+       end
+
+       def self.random_email
+         "#{random_word}@#{random_word}.#{random_word}"
+       end
+    end
